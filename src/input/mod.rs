d9b68f4aa8be45ad87507b83f5fd5a270dab0817
Skygrango <skygrango@gmail.com>
2026-06-19 19:10:04 +0800

    testing

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -506,11 +506,11 @@ impl State {
                                 .map(|(target, pos)| (target, pos.as_logical()));
 
                             // TODO: We might need a solution that allows constraints to bypass the surface without affecting the constraints themselves
-                            if new_under.as_ref().and_then(|(under, _)| under.wl_surface())
-                                != surface.wl_surface()
-                            {
-                                return (false, None);
-                            }
+                            // if new_under.as_ref().and_then(|(under, _)| under.wl_surface())
+                            //     != surface.wl_surface()
+                            // {
+                            //     return (false, None);
+                            // }
 
                             match surface {
                                 PointerFocusTarget::WlSurface { surface, .. } => {
@@ -2403,15 +2403,17 @@ impl State {
                         return false;
                     }
 
-                    with_pointer_constraint(surface, pointer, |constraint| {
-                        if let Some(constraint) = constraint
-                            && let Some(region) = constraint.region()
-                        {
-                            let point_in_surface = (p - surface_offset.to_f64()).to_i32_round();
-                            return region.contains(point_in_surface);
-                        }
-                        true
-                    })
+                    true
+
+                    // with_pointer_constraint(surface, pointer, |constraint| {
+                    //     if let Some(constraint) = constraint
+                    //         && let Some(region) = constraint.region()
+                    //     {
+                    //         let point_in_surface = (p - surface_offset.to_f64()).to_i32_round();
+                    //         return region.contains(point_in_surface);
+                    //     }
+                    //     true
+                    // })
                 };
 
                 let workspace_origin = output.geometry().loc.to_f64();
